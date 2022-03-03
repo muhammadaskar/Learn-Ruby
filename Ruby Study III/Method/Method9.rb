419f052cf5835de5a5dfb89867cf90386902bd45
Muhammad Askar <muhammadaskar74@gmail.com>
2022-03-03 17:13:23 +0700

    add material about method

---
+++ Ruby Study III/Method/Method9.rb
@@ -0,0 +1,9 @@
+# Tulis ulang parameter-parameter untuk menerima argument kata kunci
+def buy(item:, price:, count:)
+	puts "Anda telah membeli #{item} sebanyak #{count} buah"
+	puts "Total harga adalah $#{price * count}"
+end
+
+# Tulis ulang argument-argument dengan argument kata kunci
+# buy(item:"headphone", price:150, count:2)
+buy(price:150, count:2, item:"headphone")
